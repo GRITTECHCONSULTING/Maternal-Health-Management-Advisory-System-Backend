bd47a37f786d21a6dc24b6cc0f8da9b9191111d9
Christopher Amah <christopheramah0@gmail.com>
2025-12-01 10:55:12 +0100

    latest

--- mhmas/settings.py
+++ mhmas/settings.py
@@ -238,11 +238,11 @@ AUTH_USER_MODEL = "api.User"
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT", cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = "587"
+EMAIL_USE_TLS = "True"
+EMAIL_HOST_USER = "grittechconsulting@gmail.com"
+EMAIL_HOST_PASSWORD = "grittech2025mhmas"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
